bdc3dbf4db37b279cb2d7429e3693cbaa3292488
youngonesee <674180742@qq.com>
2020-07-10 18:04:26 +0800

    update

---
+++ react-demo/precache-manifest.d8549ac9cc65fc6e964a67d2a2797df0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87568b718b1dae8e78722cae217b9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7548f3536c2bf9ee2bba",
+    "url": "/static/css/main.7688ef91.chunk.css"
+  },
+  {
+    "revision": "b0622aa530ceeddb3888",
+    "url": "/static/js/2.a9f9d234.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9f9d234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7548f3536c2bf9ee2bba",
+    "url": "/static/js/main.1c9aadb1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "ba8c56eb523056ce18ea4f7075ea90aa",
+    "url": "/static/media/card.ba8c56eb.jpg"
+  }
+]);
